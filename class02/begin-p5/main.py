5c8d9f0b19725fffa22f4a7598b75492486439f1
pa-nik <76602480+pa-nik@users.noreply.github.com>
2023-02-06 11:52:59 -0800

    draw flower

--- class02/begin-p5/main.py
+++ class02/begin-p5/main.py
@@ -6,3 +6,17 @@ def setup():
 
 def draw():
     p5.background(255)           # white background
+    p5.push()
+    p5.translate(150, 150)
+    draw_flower()
+    p5.pop()
+    
+def draw_petal():
+    p5.ellipse(75, 0, 100, 25)
+
+def draw_flower():
+    for i in range(20):
+        p5.noStroke()
+        p5.fill(i*25, 255-i*25, 0)
+        p5.rotate(p5.radians(360/20))
+        draw_petal()

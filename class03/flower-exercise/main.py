9ff7e33f955d4f65beb26218305b70ef40d9e3ed
pa-nik <76602480+pa-nik@users.noreply.github.com>
2023-02-06 21:00:02 -0800

    flower exercise

--- class03/flower-exercise/main.py
+++ class03/flower-exercise/main.py
@@ -2,10 +2,10 @@ import js
 p5 = js.window
 
 def setup():
-    p5.createCanvas(300, 300)    # 300 x 300 pixel canvas 
+    p5.createCanvas(300, 300)
 
 def draw():
-    p5.background(255)           # white background
+    p5.background(255)
     # draw flower at specified coordinates:
     draw_flower(x = 100, y = 100)
     # draw another flower at smaller scale:
@@ -22,10 +22,10 @@ def draw_petal():
 # function with 2 parameters:
 def draw_flower(x, y):
     p5.push()
-    p5.translate(x, y)
-    for i in range(20):
+    p5.translate(x, y)  # move coordinates by x, y
+    for i in range(20):  # repeat 20 times
         p5.noStroke()
-        p5.fill(i*25, 255-i*25, 0, 200)
-        p5.rotate(p5.radians(360/20))
-        draw_petal()
+        p5.fill(i*25, 255-i*25, 0, 200)  # increasing red, decreasing green
+        p5.rotate(p5.radians(360/20))  # 1/20th of full rotation
+        draw_petal()  # call the function to draw petal
     p5.pop()

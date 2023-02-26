2e5c27d772c15e04b4500a171747f1efdea60198
pa-nik <76602480+pa-nik@users.noreply.github.com>
2023-02-26 13:57:02 -0800

    p5 mouse and keyboard events example

---
+++ class06/p5-events/main.py
@@ -0,0 +1,35 @@
+import js
+p5 = js.window
+
+def setup():
+    p5.createCanvas(300, 300)    # 300 x 300 pixel canvas 
+    print('finished setup..')
+
+def draw():
+    p5.background(255)           
+    p5.text(str(p5.mouseX) + ", " + str(p5.mouseY), 10, 15)
+
+def keyPressed(event):
+    print('keyPressed.. ' + str(p5.key))
+
+def keyReleased(event):
+    print('keyReleased.. ' + str(p5.key))
+
+def mousePressed(event):
+    print('mousePressed..')
+
+def mouseReleased(event):
+    print('mouseReleased..')
+
+
+
+
+
+
+
+
+
+
+
+
+

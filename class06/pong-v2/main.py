ff9075c153fc5dcb76a4fd3c4cf2f4944436c16b
pa-nik <76602480+pa-nik@users.noreply.github.com>
2023-02-28 12:24:19 -0800

    update pong v2

--- class06/pong-v2/main.py
+++ class06/pong-v2/main.py
@@ -22,9 +22,14 @@ paddle_h = 80
 left_score = 0
 right_score = 0
 
+program_state = 'START'
+program_timer = 0
+
 def setup():
     p5.createCanvas(300, 300)    # 300 x 300 pixel canvas 
     p5.rectMode(p5.CENTER)  # draw rectangles from center
+    print('program_state = ' + program_state)
+    print('click to begin..')
 
 def draw():
     p5.background(0)           # black background
@@ -32,26 +37,35 @@ def draw():
     # global variables used in this function:
     global left_score, right_score
     global ball_xspeed, ball_yspeed
+    global program_state, program_timer
 
     p5.fill(255)
     p5.textSize(18)
     
     p5.text('left: ' + str(left_score), 50, 40)
     p5.text('right: ' + str(right_score), 200, 40)
+
     draw_dotted_line()
 
-    # begin playing with a mouse click if the ball is stopped:
-    if(ball_xspeed == 0 and ball_yspeed == 0):
-        if(p5.mouseIsPressed):
-            # set random ball speed:
-            ball_xspeed = p5.random(1, 1.5)
-            ball_yspeed = p5.random(-1, 1)
-            # make the ball go in the direction of the winning player:
-            if(left_score > right_score):
-                ball_xspeed *= -1
-
-    # update and draw ball:
-    update_ball()
+    if(program_state == 'START'):
+        p5.text('click to begin...', 100, 265)
+    if(program_state == 'PLAY'):
+        update_ball()  # update ball position during PLAY only
+    elif(program_state == 'RESET'):
+        if(p5.millis() > program_timer + 3000):
+            print('end RESET after 3 seconds..')
+            program_state = 'PLAY'
+            print('change program_state to ' + program_state)
+            # begin playing if the ball is stopped:
+            if(ball_xspeed == 0 and ball_yspeed == 0):
+                # set random ball speed:
+                ball_xspeed = p5.random(1, 1.5)
+                ball_yspeed = p5.random(-1, 1)
+                # make the ball go in the direction of the winning player:
+                if(left_score > right_score):
+                    ball_xspeed *= -1
+    
+    # draw ball:
     draw_ball(ball_x, ball_y)
     
     # update and draw player paddles:
@@ -63,10 +77,12 @@ def draw():
     wall_hit = check_walls_hit()  # check if a wall was hit 
     if(wall_hit != None):
         if(wall_hit == 'right'):  # right wall hit
-            left_score += 1  # left player scores
+            print('left player scores..')
+            left_score += 1  
             reset_game()
         elif(wall_hit == 'left'):  # left wall hit
-            right_score += 1  # right player scores
+            print('right player scores..')
+            right_score += 1  
             reset_game()
 
     # check if player paddle is hit and bounce ball:
@@ -78,10 +94,14 @@ def draw():
 def reset_game():
     global ball_x, ball_y
     global ball_xspeed, ball_yspeed
+    global program_state, program_timer
     ball_x = 150
     ball_y = 150
     ball_xspeed = 0
     ball_yspeed = 0
+    program_state = 'RESET'
+    print('change program_state to ' + program_state)
+    program_timer = p5.millis()
 
 # draw dotted vertical line at the center of canvas:
 def draw_dotted_line():
@@ -162,3 +182,21 @@ def check_paddles_hit():
     else:
         return None  # no paddles were hit
 
+def keyPressed(event):
+    pass  
+
+def keyReleased(event):
+    pass
+
+def mousePressed(event):
+    global program_state
+    if(program_state == 'START'):
+        program_state = 'RESET'
+    elif(program_state == 'PLAY'):
+        program_state = 'PAUSE'
+    else:
+        program_state = 'PLAY'
+    print('change program_state to ' + program_state)
+
+def mouseReleased(event):
+    pass

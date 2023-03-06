e72a6752d1f1571a843ff7f82e5824ce2d502a37
pa-nik <76602480+pa-nik@users.noreply.github.com>
2023-03-06 10:57:07 -0800

    traffic light exercise

---
+++ class07/traffic-light-exercise/main.py
@@ -0,0 +1,44 @@
+import js
+p5 = js.window
+
+class TrafficLight:
+    def __init__(self):
+        self.state = 'green'
+        self.timer = 0
+    
+    def update(self):
+        if(self.state == 'green'):
+            self.state = 'yellow'
+        elif(self.state == 'yellow'):
+            self.state = 'red'
+        elif(self.state == 'red'):
+            self.state = 'green'
+        
+    def draw(self):
+        p5.push()
+        p5.translate(p5.width/2, p5.height/2)
+        if(self.state == 'red'):
+            p5.fill(255, 0, 0)
+        else:
+            p5.fill(150, 0, 0)
+        p5.ellipse(0, -p5.height/4, 60, 60)
+        p5.pop()
+
+def setup():
+    p5.createCanvas(300, 300)
+	    
+def draw():
+    p5.background(0)
+    traffic_light.draw()
+
+def keyPressed(event):
+    pass 
+
+def keyReleased(event):
+    pass
+
+def mousePressed(event):
+    pass
+
+def mouseReleased(event):
+    pass

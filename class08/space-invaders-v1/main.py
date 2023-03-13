6bbc1e124441ee4ca691989bc00fa7df9d6024d8
pa-nik <76602480+pa-nik@users.noreply.github.com>
2023-03-13 10:43:01 -0700

    class08 space invaders template

---
+++ class08/space-invaders-v1/main.py
@@ -0,0 +1,33 @@
+import js
+p5 = js.window
+
+'''
+# images and fonts available to use in this program:
+invader_1.png
+invader_2.png
+spaceship.png
+PressStart2P.otf
+'''
+
+def setup():
+    p5.createCanvas(300, 300)  
+    p5.rectMode(p5.CENTER)
+    p5.imageMode(p5.CENTER)
+
+def draw():
+    p5.background(0)
+
+def keyPressed(event):
+    pass
+
+def keyReleased(event):
+    pass
+
+def mousePressed(event):
+    pass
+
+def mouseReleased(event):
+    pass
+
+
+    

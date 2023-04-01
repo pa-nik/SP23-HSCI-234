91f5eebd2f4c988c186d17ff67aa7e65ab277b17
pa-nik <76602480+pa-nik@users.noreply.github.com>
2023-04-01 15:11:07 -0700

    update sound example

--- extras/sound-example/main.py
+++ extras/sound-example/main.py
@@ -1,10 +1,13 @@
 import js
 p5 = js.window
 
-sound = None
+print('click on canvas to play sound..')
+
+sound = None  # variable to store sound data
 
 def setup():
     p5.createCanvas(300, 300) 
+    # load sound file data and assign to the global sound variable:
     global sound
     sound = p5.loadSound('shoot.wav')
     print('finished setup') 
